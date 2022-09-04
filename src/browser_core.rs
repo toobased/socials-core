56c24545d797b110666974d862d6a982311da9d9
worlddeleteRin <worlddelete0@gmail.com>
2022-09-04 14:19:48 +0300

    🐛 possible fix of free process port assigning

--- src/browser_core.rs
+++ src/browser_core.rs
@@ -1,6 +1,6 @@
 use std::net::TcpListener;
 
-use log::info;
+use log::debug;
 use fantoccini::ClientBuilder;
 use tokio::process;
 
@@ -16,13 +16,21 @@ impl BrowserCore {
     pub async fn new () -> Self { Self::init().await }
 
     fn get_free_port () -> Option<String> {
+        /*
         for port in 4446..5000 {
+            debug!("binding to {}", port);
             match TcpListener::bind(("127.0.0.1", port)) {
-                Ok(_) => return Some(port.to_string()),
+                Ok(l) => return Some(l),
                 _ => {}
             }
         }
-        None
+        */
+        // just spawn listener on free port
+        let listener = TcpListener::bind(("127.0.0.1", 0));
+        match listener {
+            Ok(l) => Some(l.local_addr().unwrap().port().to_string()),
+            _ => None
+        }
     }
 
     fn init_driver (port: &str) -> process::Child {
@@ -59,12 +67,12 @@ impl BrowserCore {
 
         let mut url: String = "http://localhost:".to_string();
         url.push_str(port);
-        info!("connection url is {}", url);
+        debug!("connection url is {}", url);
         let client = client
             .connect(&url)
             .await
             .expect("Cant connect to browser client");
-        info!("Browser client initalized on port 'port_here'");
+        debug!("Browser client initalized on port {}", port);
         client
     }
 
@@ -73,13 +81,14 @@ impl BrowserCore {
         // let listener = Self::get_listener().expect("Cant raise listener.");
         // listener.set_nonblocking(true).unwrap();
         // let port = listener.local_addr().unwrap().port().to_string();
-        let port = BrowserCore::get_free_port().expect("Cant get free port");
-        info!("Port will be used: {}", port);
-        let webdriver = Self::init_driver(&port);
+        let port_string = BrowserCore::get_free_port().expect("Cant get free port");
+        // let port_string = port.local_addr().unwrap().port().to_string().clone();
+        debug!("Port will be used: {}", port_string);
+        let webdriver = Self::init_driver(&port_string);
         Self {
             webdriver_process: webdriver,
             // listener,
-            client: Self::init_client(&port).await
+            client: Self::init_client(&port_string).await
         }
     }
 
